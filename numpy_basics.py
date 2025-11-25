647725b9968ef65a93c24fcfd4120a03dbccc507
gokulkrishna19 <gokulkrishna123z@gmail.com>
2025-11-25 23:53:40 +0530

    Create numpy_basics.py

---
+++ numpy_basics.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+arr = np.array([10, 20, 30, 40])
+print("Array:", arr)
+print("Mean:", np.mean(arr))
+print("Standard Deviation:", np.std(arr))
+print("Reshaped (2x2):\n", arr.reshape(2, 2))
