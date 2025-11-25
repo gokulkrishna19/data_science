7489263a20e95f9d19e9a309d9b7cf77a3057237
gokulkrishna19 <gokulkrishna123z@gmail.com>
2025-11-26 00:02:15 +0530

    Create matplotlib_scatter_plot.py

---
+++ matplotlib_scatter_plot.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+x = [5, 7, 8, 7, 2, 17, 2, 9]
+y = [99, 86, 87, 88, 100, 86, 103, 87]
+
+plt.scatter(x, y)
+plt.title("Scatter Plot Example")
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.show()
