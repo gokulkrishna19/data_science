0a1b18b827e9aa16200ebbfa19203ab698439120
gokulkrishna19 <gokulkrishna123z@gmail.com>
2025-11-26 00:03:22 +0530

    Add pie chart example using matplotlib

---
+++ matplotlib_pie_chart.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+
+sizes = [40, 30, 20, 10]
+labels = ["A", "B", "C", "D"]
+
+plt.pie(sizes, labels=labels, autopct="%1.1f%%")
+plt.title("Pie Chart Example")
+plt.show()
