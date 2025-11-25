451c0eb1ad9657e138ac2af26d7202a01122a040
gokulkrishna19 <gokulkrishna123z@gmail.com>
2025-11-25 23:57:13 +0530

    Create matplotlib_line_plot.py

---
+++ matplotlib_line_plot.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+x = [1, 2, 3, 4, 5]
+y = [10, 15, 8, 12, 20]
+
+plt.plot(x, y)
+plt.title("Simple Line Plot")
+plt.xlabel("X Axis")
+plt.ylabel("Y Axis")
+plt.show()
